f7b621c84c90863151d0ccb94558bd8cec151a80
Samuel Toshikazu Oizume <samuel.oizume@gmail.com>
2020-11-29 23:29:58 -0300

    #29-11-2020 Realizar atualizacao

--- src/app.js
+++ src/app.js
@@ -4,25 +4,11 @@ require('dotenv').config({
 var express = require('express');
 var bodyParser = require('body-parser'); 
 var multer = require('multer');
-var cors = require('cors');
 var form = multer();
 const app = express();
 const routes = require('./routes');
 
 require('./database');
-
-var corsOptions = {
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
-    }
-}
-
-app.use(cors(corsOptions));
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const routes = express.Router();
-
+const cors = require('cors')
 const LoginController = require('../controllers/LoginController');
 const ContatoController = require('../controllers/ContatoController'); 
 
-routes.get('/usuario/auth', LoginController.authentication);
-routes.post('/usuario/save', LoginController.save);
-routes.put('/usuario/update/:id', LoginController.update);
-routes.delete('/usuario/delete/:id', LoginController.delete);
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
 
-routes.post('/contato/save', ContatoController.save);
-routes.put('/contato/update/:id', ContatoController.update);
-routes.get('/contato/list', ContatoController.findAll);
-routes.get('/contato/findOne', ContatoController.findById);
-routes.delete('/contato/delete/:id', ContatoController.delete);
+routes.get('/usuario/auth',cors(corsOptions), LoginController.authentication);
+routes.post('/usuario/save',cors(corsOptions), LoginController.save);
+routes.put('/usuario/update/:id',cors(corsOptions), LoginController.update);
+routes.delete('/usuario/delete/:id',cors(corsOptions), LoginController.delete);
+
+routes.post('/contato/save', cors(corsOptions), ContatoController.save);
+routes.put('/contato/update/:id',cors(corsOptions), ContatoController.update);
+routes.get('/contato/list',cors(corsOptions), ContatoController.findAll);
+routes.get('/contato/findOne',cors(corsOptions), ContatoController.findById);
+routes.delete('/contato/delete/:id',cors(corsOptions), ContatoController.delete);
 
 module.exports = routes;  
