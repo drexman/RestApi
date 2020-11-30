e13d517e5cea962cdc16642441b469b9a31db7d3
Samuel Toshikazu Oizume <samuel.oizume@gmail.com>
2020-11-29 22:44:49 -0300

    #29-11-2020 Realiza atualizacao

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(express.static('public'));
 app.use((req, res, next) => {
 	
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Methods",'GET,PUT,POST,DELETE');
     app.use(cors());
     next();
 });
